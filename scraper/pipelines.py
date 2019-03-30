511ca8880d2e2f3f40b291409411c6d8a11c5f96
Evan Buss <evan.buss28@gmail.com>
2019-03-29 23:43:33 -0400

    IMDB Scraper Basic Implementation - Saves the results to a SQLite database file

---
+++ scraper/pipelines.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import sqlite3
+import logging
+
+
+class MovieToDBPipeLine(object):
+    def __init__(self):
+        self.connection = sqlite3.connect(
+            'movies.db',
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(
+            'CREATE TABLE IF NOT EXISTS movies'
+            ' (title TEXT PRIMARY KEY, year INT, rating REAL)'
+        )
+
+    def process_item(self, item, spider):
+        self.cursor.execute(
+            'SELECT * FROM movies WHERE title = ?', (item['title'],)
+        )
+
+        result = self.cursor.fetchone()
+        if result:
+            logging.warning(f"{item['title']} is already in the database")
+        else:
+            self.cursor.execute(
+                'INSERT INTO movies (title, year, rating)'
+                ' VALUES (?, ?, ?)',
+                (item['title'], item['year'], item['rating'])
+            )
+            self.connection.commit()
+            logging.info(f"{item['title']} added to the database")
+        return item
---
+++ scraper/spiders/movies_spider.py
@@ -0,0 +1,25 @@
+# This spider will extract movies from IMDB
+
+import scrapy
+import unicodedata
+import sqlite3
+
+
+class MoviesSpider(scrapy.Spider):
+    name = 'movies'
+
+    start_urls = ['https://www.imdb.com/title/tt0944947/']
+
+    def parse(self, response):
+        yield {
+            'title': str.strip(unicodedata.normalize('NFKD', response.css('div.title_wrapper h1::text').get())),
+            'year': response.css('span#titleYear a::text').get(),
+            'rating': response.css('div.ratingValue strong span::text').get()
+        }
+
+        # Return an array of all recommended movie links
+        new_links = response.css('div.rec_item a::attr(href)').getall()
+
+        # Basically performs a depth first search following each link
+        for link in new_links:
+            yield response.follow(link, callback=self.parse)
